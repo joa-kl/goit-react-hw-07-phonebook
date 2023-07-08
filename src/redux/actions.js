4a67c0a6f63013612774a735024f7370d7db99e8
Joanna K <joannakleinrm@gmail.com>
2023-07-08 19:14:43 +0200

    .

---
+++ src/redux/actions.js
@@ -0,0 +1,41 @@
+import { nanoid } from "@reduxjs/toolkit"
+
+export const toggleCompleted = (taskId) => {
+    return {
+        type: "tasks/toggleCompleted",
+        payload: taskId,
+    }
+}
+
+export const deleteTask = taskId => {
+    return {
+        type: "tasks/deleteTask",
+        payload: taskId
+    };
+};
+
+export const addTask = (text) => {
+    return {
+        type: "tasks/addTask",
+        payload: {
+            id: nanoid(),
+            text,
+            completed: false,
+        }
+    }
+}
+
+
+export const setStatusFilter = value => {
+    return {
+        type: "filters/setStatusFilter",
+        payload: value,
+    };
+};
+
+// export const setStatusFilter = value => {
+//     return {
+//         type: "filters/setStatusFilter",
+//         payload: value,
+//     };
+// };
